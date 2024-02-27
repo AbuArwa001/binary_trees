2e0cf25362272dd5006d49428283acf1a5f65177
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-28 00:09:22 +0300

    check if is root

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,10 @@ void binary_tree_delete(binary_tree_t *tree);
  * Return: return 1 if node is leaf otherwise 0
 */
 int binary_tree_is_leaf(const binary_tree_t *node);
+/**
+ * binary_tree_is_root -  checks if a node is a root
+ * @node: Pointer to the node to check
+ * Return: return 1 if node is leaf otherwise 0
+*/
+int binary_tree_is_root(const binary_tree_t *node);
 #endif
