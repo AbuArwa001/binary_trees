ddd4bc779f6c008a3be9a2b0d897bb80a7470618
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-29 19:09:59 +0300

    Level order

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -14,38 +14,5 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	}
 
-	/*create a queue */
-	Queue *q = createQueue();
 
-	if (!q)
-	{
-		return;
-	}
-
-	/* enqueue the root node of the tree */
-	enqueue(q, (void *)tree);
-
-	while (!isEmpty(q))
-	{
-		/* dequeue the front node of the queue */
-		binary_tree_t *node = dequeue(q);
-		/* call the func function on the node's value */
-		func(node->n);
-
-		/* if the node has a left chil */
-		if (node->left)
-		{
-			/* enqueue the left child to the queue */
-			enqueue(q, (void *)node->left);
-		}
-
-		/* if the node has a right chil */
-		if (node->right)
-		{
-			/* enqueue the right child to the queue */
-			enqueue(q, (void *)node->right);
-		}
-	}
-
-	destroyQueue(q);
 }
