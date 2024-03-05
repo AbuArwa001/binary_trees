88ee441f8bb76ae817f74c5f74a2267432c72e58
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-06 02:01:17 +0300

    Header and print

--- binary_trees.h
+++ binary_trees.h
@@ -192,6 +192,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 /**
  * binary_tree_is_complete - checks if a binary tree is complete
  * @tree: pointer to the root node of the tree to check
+ * Return: return 1 if complete btree or 0 if not
 */
 int binary_tree_is_complete(const binary_tree_t *tree);
 #endif
