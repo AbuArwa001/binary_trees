8145f5201288d5e5729219db9e368b0ab1d763d0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-06 01:52:30 +0300

    Check if tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,93 +1,90 @@
 #include "binary_trees.h"
+#include "101-binary_tree_levelorder.c"
 #include <stdio.h>
 
 /**
- * max_depth - finds the max depth of each subtree
- * @tree: pointer to node of subtree
- * Return: returns the max_depth
-*/
-size_t max_depth(const binary_tree_t *tree)
+ * enqueue - Adds a node to the rear of the queue
+ * @q: Pointer to the queue
+ * @tree: Pointer to the binary tree node to enqueue
+ * @fr: pointer to front and rear
+ * Return: returns queue node
+ */
+queue_t *enqueue(const binary_tree_t *tree, queue_t *q, Queue_t *fr)
 {
-	if (!tree)
+	queue_t *new_node = malloc(sizeof(queue_t));
+
+	new_node->node = (binary_tree_t *) tree;
+	new_node->next = NULL;
+
+	if (!fr->front && !fr->rear)
 	{
-		return (0);
+		fr->front = fr->rear =  new_node;
+		return (new_node);
+	}
+
+	fr->rear->next =  new_node;
+	fr->rear =  new_node;
+	return (q);
+}
+/**
+ * dequeue - Removes and returns the node at the front of the queue
+ * @fr: Pointer to the front and rear
+ * Return: Pointer to the dequeued binary tree node,
+ *          or NULL if the queue is empty
+ */
+binary_tree_t *dequeue(Queue_t *fr)
+{
+	if (!fr->front)
+	{
+		return (NULL);
+	}
+
+	binary_tree_t *data = fr->front->node;
+	queue_t *temp = fr->front;
+
+	if (fr->front == fr->rear)
+	{
+		fr->front = fr->rear = NULL;
 	}
 	else
 	{
-		size_t lh = max_depth(tree->left);
-		size_t rh = max_depth(tree->right);
-
-		return (lh > rh ? (lh + 1) : (rh + 1));
+		fr->front = fr->front->next;
 	}
-}
-queue_t *enqueue(const binary_tree_t *tree, queue_t *q, Queue_t *fr)
-{
-    queue_t *new_node = malloc(sizeof(queue_t));
 
-    new_node->node =(binary_tree_t *) tree;
-    new_node->next = NULL;
-
-    if (!fr->front && !fr->rear)
-    {
-        fr->front = fr->rear =  new_node;
-        return new_node;
-    }
-    fr->rear->next =  new_node;
-    fr->rear =  new_node;
-    return (q);
-}
-
-binary_tree_t *dequeue(Queue_t *fr)
-{
-    if (!fr->front)
-    {
-        return NULL;
-    }
-    binary_tree_t *data = fr->front->node;
-    queue_t *temp = fr->front;
-    if (fr->front == fr->rear)
-    {
-        fr->front = fr->rear = NULL;
-    }
-    else
-    {
-        fr->front = fr->front->next;
-    }
-    free(temp);
-    return data;
+	free(temp);
+	return (data);
 }
 
 /**
- * printCurrentLevel - prints current level
+ * CurrentLevel - prints current level
  * @tree: pointer to node of subtree
  * @level: current level
  * @q: pointer to the queue
  * @fr: pointer to the queue structure
 */
-void CurrentLevel(const binary_tree_t *tree, int level,  queue_t **q, Queue_t *fr)
+void CurrentLevel(const binary_tree_t *tree, int level,  queue_t **q,
+		Queue_t *fr)
 {
 	if (tree == NULL)
-    {
-        printf("NULL");
-        *q = enqueue(tree, *q, fr);
-        return;
-    }
+	{
+		*q = enqueue(tree, *q, fr);
+		return;
+	}
 
 	if (level == 1)
-    {
-        // Enqueue the node to the queue and update the queue pointer
-        *q = enqueue(tree, *q, fr);
-        printf("%d ", tree->n);
-    }
-	else if (level > 1)
-    {
-        CurrentLevel(tree->left, level - 1,  q, fr);
-        CurrentLevel(tree->right, level - 1, q, fr);
-    }
+	{
+		*q = enqueue(tree, *q, fr);
+	}
+	else
+		if (level > 1)
+		{
+			CurrentLevel(tree->left, level - 1,  q, fr);
+			CurrentLevel(tree->right, level - 1, q, fr);
+		}
 }
 
 /**
- * printLevelOrder - Function to print level order traversal a tree
+ * levelOrder - Function to print level order traversal a tree
  * @tree: pointer to node of subtree
  * @q: pointer to the queue
  * @fr: pointer to the queue structure
@@ -100,40 +97,51 @@ void levelOrder(const binary_tree_t *tree,  queue_t **q, Queue_t *fr)
 	for (i = 1; i <= h; i++)
 		CurrentLevel(tree, i, q, fr);
 }
-Queue_t *createQueue()
-{
-    Queue_t *qu = malloc(sizeof(Queue_t));
-    qu->front = NULL;
-    qu->rear = NULL;
-    return (qu);
-}
 /**
  * binary_tree_is_complete - checks if a binary tree is complete
  * @tree: pointer to the root node of the tree to check
+ * Return: return 1 if complete btree or 0 if not
 */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-    queue_t *q = NULL;
-    Queue_t *fr = createQueue();
-    levelOrder(tree,  &q, fr);
-    while (fr->rear)
-    {
-        binary_tree_t *pr = dequeue(fr);
-        printf("%d ", pr->n);  
-        if (pr && fr->rear)
-        {
-            
-        }
-        else if (!pr && fr->rear)
-        {
-            return (0);
-        }
+	if (!tree)
+		return (0);
 
-    }
-    // printf("END OF A TREE\n");
-    return (1);
+	queue_t *q = NULL;
+	int flg = -1;
+	Queue_t *fr = malloc(sizeof(Queue_t));
+
+	if (!fr)
+		return (0);
+	fr->front = NULL;
+	fr->rear = NULL;
+	levelOrder(tree,  &q, fr);
+
+	while (fr->rear)
+	{
+
+		binary_tree_t *pr = dequeue(fr);
+
+		if (!pr)
+		{
+			flg = 1;
+		}
+		else
+			if (flg == 1 && pr)
+			{
+				flg = 0;
+				break;
+			}
+	}
+
+	if ((!fr->front && !fr->rear) && flg == -1)
+		return (1);
+	if (flg == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
