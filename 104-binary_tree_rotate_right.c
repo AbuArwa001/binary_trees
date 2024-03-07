453c0d0a8e3fb1b28057ade98d30eb96b57a440e
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-07 18:25:46 +0300

    Rotate right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,28 @@
+/**
+ * binary_tree_rotate_right - rotate sub tree right
+ * @tree: sub tree to be rotated
+ * Return: return the new sub tree root
+*/
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *node = NULL;
+
+	if (!tree || !tree->left)
+	{
+		return (NULL);
+	}
+
+	node = tree->left;
+
+	tree->left = node->right;
+
+	if (node->right)
+	{
+		node->right->parent = tree;
+	}
+
+	node->right = tree;
+	node->parent = tree->parent;
+	tree->parent = node;
+	return (node);
+}
